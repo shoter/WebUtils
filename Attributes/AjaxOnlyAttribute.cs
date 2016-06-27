caaeead7747c63eac4011ffc5d84677981a5d91e
Damian <lubiebardzozelki@gmail.com>
2016-06-27 21:23:53 +0200

    First commit

---
+++ Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WebUtils.Attributes
+{
+    public class AjaxOnlyAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, System.Reflection.MethodInfo methodInfo)
+        {
+            return controllerContext.RequestContext.HttpContext.Request.IsAjaxRequest();
+        }
+    }
+
+}
---
+++ PagingParams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebUtils
+{
+    public class PagingParams
+    {
+        public int Page { get; set; }
+        public int Records { get; set; } = 0;
+        public int RecordsPerPage { get; set; } = 10;
+        public int PageCount
+        {
+            get
+            {
+                return (int)Math.Ceiling(Records / (double)RecordsPerPage);
+            }
+        }
+
+
+        public PagingParams() { }
+
+
+        public IQueryable<T> Apply<T>(IQueryable<T> query)
+        {
+            Page = query.Count();
+
+            return query.Skip((PageCount - 1) * RecordsPerPage)
+                .Take(RecordsPerPage);
+        }
+
+
+    }
+}
