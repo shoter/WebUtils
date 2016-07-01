b81e5ce3f3c8e307555ee02712804d01d1c621dd
Damian Łączak <Damian czak>
2016-07-02 00:34:10 +0200

    Added RouteDataHelper

---
+++ Helpers/RouteDataHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Routing;
+
+namespace WebUtils.Helpers
+{
+    public static class RouteDataHelper
+    {
+        public static RouteValueDictionary RouteValueDictionary
+        {
+            get
+            {
+                return HttpContext.Current.Request.RequestContext.RouteData.Values;
+            }
+        }
+
+        public static string ActionName
+        {
+            get
+            {
+                var routeValues = HttpContext.Current.Request.RequestContext.RouteData.Values;
+                if (routeValues != null)
+                {
+                    if (routeValues.ContainsKey("action"))
+                    {
+                        return HttpContext.Current.Request.RequestContext.RouteData.Values["action"].ToString();
+                    }
+                }
+
+                throw new KeyNotFoundException();
+            }
+        }
+
+        public static string ControllerName
+        {
+            get
+            {
+                var routeValues = HttpContext.Current.Request.RequestContext.RouteData.Values;
+                if (routeValues != null)
+                {
+                    if (routeValues.ContainsKey("controller"))
+                    {
+                        return HttpContext.Current.Request.RequestContext.RouteData.Values["controller"].ToString();
+                    }
+                }
+
+                throw new KeyNotFoundException();
+            }
+        }
+    }
+}
