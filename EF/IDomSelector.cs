df0629d1f35b0807e5279f0a2dd2de4192892f47
Damian Łączak <Damian czak>
2017-01-21 11:37:46 +0100

    Added IDomSelector.

---
+++ EF/IDomSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebUtils.EF
+{
+    public interface IDomSelector<TEntity, TResult>
+        where TEntity : class
+    {
+        IQueryable<TResult> Select(IQueryable<TEntity> query);
+    }
+}
--- Extensions/IQueryableExtensions.cs
+++ Extensions/IQueryableExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebUtils.EF;
 
 namespace WebUtils.Extensions
 {
@@ -11,6 +12,12 @@ namespace WebUtils.Extensions
         public static IQueryable<T> Apply<T>(this IQueryable<T> query, PagingParam pp)
         {
             return pp.Apply(query);
+        }
+
+        public static IQueryable<TSelect> Apply<T, TSelect>(this IQueryable<T> query, IDomSelector<T, TSelect> selector)
+            where T : class
+        {
+            return selector.Select(query);
         }
     }
 }
