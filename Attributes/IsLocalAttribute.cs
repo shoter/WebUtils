00c10b91205f559f1c91de205ff5ad46f537395a
Damian Łączak <Damian czak>
2017-02-26 11:00:33 +0100

    Added IsLocal attribute.

---
+++ Attributes/IsLocalAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WebUtils.Attributes
+{
+    public class IsLocalAttribute : ActionMethodSelectorAttribute
+    {
+        public bool Local { get; set; }
+        public IsLocalAttribute(bool local = true)
+        {
+            Local = local;
+        }
+
+        public override bool IsValidForRequest(ControllerContext controllerContext, System.Reflection.MethodInfo methodInfo)
+        {
+            return controllerContext.RequestContext.HttpContext.Request.IsLocal == Local;
+        }
+    }
+}
